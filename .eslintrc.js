60649c22088f4e27e3465bf0d1d9da5663516621
leechikit <leechikit0823@163.com>
2019-06-10 15:25:55 +0800

    update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'no-console': 'off', // 允许使用 console
     quotes: ['error', 'single'], // 必须使用单引号，禁止使用双引号
     semi: 'off', // 结尾必须没有分号
+    'space-before-function-paren': ["error", "never"], // function前不需要加空格
     'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }] // vue模板不检查闭合标签
   }
 }
